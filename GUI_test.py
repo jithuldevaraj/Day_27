3b78f735c24ac57c64c81300677359c07ab385cb
Jithu <kitetvmsouth@gmail.com>
2026-06-18 23:04:18 +0530

    Add all widgets in one file.

--- GUI_test.py
+++ GUI_test.py
@@ -10,7 +10,7 @@ window_1.geometry("500x300")
 label = tkinter.Label(window_1, text="Test_label")
 label.place(x=5, y=14)
 
-
+# Button
 # Function to reset the label text
 def reset_label():
     label["text"] = "Test_label"
@@ -19,9 +19,8 @@ def button_cmd():
     # 1. Change the text immediately
     label["text"] = "Button clicked"
     # 2. Schedule the reset_label function to run after 2000 ms (2 seconds)
-    window_1.after(2000, reset_label)
+    window_1.after(2000, reset_label)  # notes:- GUI_1
 
-# Button
 button = tkinter.Button(window_1, text="Click Me", command=button_cmd)
 button.place(x=400, y=14)
 
